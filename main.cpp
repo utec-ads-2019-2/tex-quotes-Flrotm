b6319c55eb7304cd9bfef88ac9505975b36232d6
Flrotm <39736679+Flrotm@users.noreply.github.com>
2019-08-25 22:02:23 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-int main(int argc, char *argv[]) {
-    return 0;
+
+int main(){
+string str;
+int x=0;
+while(getline(cin,str)){
+
+    while(str.find('"')+1){
+        if(x==0){
+            str.replace(str.find('"'),1,"``");
+            x=1;
+        }
+        else{
+            str.replace(str.find('"'),1,"''");
+            x=0;
+        }
+    }
+    cout<<str<<endl;
+}
+
 }
